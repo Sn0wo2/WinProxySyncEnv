264b968e22c51ba037c9bbbb2b7ac572b4f862b9
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-08-01 23:46:39 +0800

    refactor(utils): add
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

---
+++ utils/ProxyUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WinProxyEnvSync.utils
+{
+    public static class ProxyUtils
+    {
+        public enum SendMessageTimeoutFlags : uint
+        {
+            SMTO_NORMAL = 0x0,
+            SMTO_BLOCK = 0x1,
+            SMTO_ABORTIFHUNG = 0x2,
+            SMTO_NOTIMEOUTIFNOTHUNG = 0x8
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr SendMessageTimeout(
+            IntPtr hWnd,
+            uint Msg,
+            UIntPtr wParam,
+            string lParam,
+            SendMessageTimeoutFlags fuFlags,
+            uint uTimeout,
+            out UIntPtr lpdwResult);
+    }
+}
