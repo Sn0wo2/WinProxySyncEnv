3f8aedf63c3932b18c3421be43c7abc31fd08405
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-09-27 06:48:28 +0800

    refactor(utils): proxy
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- utils/ProxyUtils.cs
+++ utils/ProxyUtils.cs
@@ -7,36 +7,45 @@ namespace WinProxyEnvSync.utils;
 
 public static class ProxyUtils
 {
-    public enum SendMessageTimeoutFlags : uint
+  public enum SendMessageTimeoutFlags : uint
+  {
+    SMTO_NORMAL = 0x0,
+    SMTO_BLOCK = 0x1,
+    SMTO_ABORTIFHUNG = 0x2,
+    SMTO_NOTIMEOUTIFNOTHUNG = 0x8
+  }
+
+  private static readonly string RegistryPath = @"Software\Microsoft\Windows\CurrentVersion\Internet Settings";
+
+  [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+  public static extern IntPtr SendMessageTimeout(
+    IntPtr hWnd,
+    uint Msg,
+    UIntPtr wParam,
+    string lParam,
+    SendMessageTimeoutFlags fuFlags,
+    uint uTimeout,
+    out UIntPtr lpdwResult);
+
+  public static ProxyInfo GetCurrentInfo()
+  {
+    try
     {
-        SMTO_NORMAL = 0x0,
-        SMTO_BLOCK = 0x1,
-        SMTO_ABORTIFHUNG = 0x2,
-        SMTO_NOTIMEOUTIFNOTHUNG = 0x8
+      using var key = Registry.CurrentUser.OpenSubKey(RegistryPath, false);
+
+      if (key?.GetValue("ProxyEnable") is not int proxyEnableValue)
+        return new ProxyInfo(false, null, null);
+
+      var proxyEnable = proxyEnableValue == 1;
+
+      var proxyServer = proxyEnable ? key.GetValue("ProxyServer") as string : null;
+      var proxyOverride = proxyEnable ? key.GetValue("ProxyOverride") as string : null;
+
+      return new ProxyInfo(proxyEnable, proxyServer, proxyOverride);
     }
-
-    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    public static extern IntPtr SendMessageTimeout(
-        IntPtr hWnd,
-        uint Msg,
-        UIntPtr wParam,
-        string lParam,
-        SendMessageTimeoutFlags fuFlags,
-        uint uTimeout,
-        out UIntPtr lpdwResult);
-
-    public static ProxyInfo GetCurrentInfo()
+    catch (Exception)
     {
-        using var key = Registry.CurrentUser.OpenSubKey(
-            @"Software\Microsoft\Windows\CurrentVersion\Internet Settings");
-
-        if (key?.GetValue("ProxyEnable") is not int proxyEnableValue)
-            return new ProxyInfo(false, null, null);
-
-        var proxyEnable = proxyEnableValue == 1;
-        return new ProxyInfo(
-            proxyEnable,
-            proxyEnable ? key.GetValue("ProxyServer") as string : null,
-            proxyEnable ? key.GetValue("ProxyOverride") as string : null);
+      return new ProxyInfo(false, null, null);
     }
+  }
 }
